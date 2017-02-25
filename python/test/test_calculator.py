72a6bd7178d4f3798ebd111d141f37afdf5fc2dc
scottpschaffer <scottpschaffer@yahoo.com>
2017-02-25 16:29:31 -0600

    fixed problem with Addition

--- python/test/test_calculator.py
+++ python/test/test_calculator.py
@@ -2,7 +2,10 @@ import calculator
 
 
 def test_add():
-    assert calculator.add(1, 1) == 2
+    assert calculator.add(3, 9) == 12
+
+def test_add_bad():
+    assert calculator.add(3, 9) == 13
 
 def test_multiply():
     assert calculator.multiply(2, 2) == 4
